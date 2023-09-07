d17891390df5b36da952affed24ff34cd443d963
Tim17301 <lorenz-tim@outlook.de>
2023-09-07 12:54:55 +0200

    content for homepage responsive

--- script.js
+++ script.js
@@ -6,49 +6,49 @@
 
 
   //Galerie Hover Effekt
-  
-  $(".galerie-container .image1").hover(function(){
-    $(".galerie-container .image1 > div").css("display", "block");
-  }, function(){
-    $(".galerie-container .image1 > div").css("display", "none");
-  });
+  if ($(window).width() > 1000){
+    $(".galerie-container .image1").hover(function(){
+      $(".galerie-container .image1 > div").css("display", "block");
+    }, function(){
+      $(".galerie-container .image1 > div").css("display", "none");
+    });
 
-  $(".galerie-container .image2").hover(function(){
-    $(".galerie-container .image2 > div").css("display", "block");
-  }, function(){
-    $(".galerie-container .image2 > div").css("display", "none");
-  });
+    $(".galerie-container .image2").hover(function(){
+      $(".galerie-container .image2 > div").css("display", "block");
+    }, function(){
+      $(".galerie-container .image2 > div").css("display", "none");
+    });
 
-  $(".galerie-container .image3").hover(function(){
-    $(".galerie-container .image3 > div").css("display", "block");
-  }, function(){
-    $(".galerie-container .image3 > div").css("display", "none");
-  });
+    $(".galerie-container .image3").hover(function(){
+      $(".galerie-container .image3 > div").css("display", "block");
+    }, function(){
+      $(".galerie-container .image3 > div").css("display", "none");
+    });
 
-  $(".galerie-container .image4").hover(function(){
-    $(".galerie-container .image4 > div").css("display", "block");
-  }, function(){
-    $(".galerie-container .image4 > div").css("display", "none");
-  });
+    $(".galerie-container .image4").hover(function(){
+      $(".galerie-container .image4 > div").css("display", "block");
+    }, function(){
+      $(".galerie-container .image4 > div").css("display", "none");
+    });
 
-  $(".galerie-container .image5").hover(function(){
-    $(".galerie-container .image5 > div").css("display", "block");
-  }, function(){
-    $(".galerie-container .image5 > div").css("display", "none");
-  });
+    $(".galerie-container .image5").hover(function(){
+      $(".galerie-container .image5 > div").css("display", "block");
+    }, function(){
+      $(".galerie-container .image5 > div").css("display", "none");
+    });
 
-  $(".galerie-container .image6").hover(function(){
-    $(".galerie-container .image6 > div").css("display", "block");
-  }, function(){
-    $(".galerie-container .image6 > div").css("display", "none");
-  });
+    $(".galerie-container .image6").hover(function(){
+      $(".galerie-container .image6 > div").css("display", "block");
+    }, function(){
+      $(".galerie-container .image6 > div").css("display", "none");
+    });
 
-  $(".galerie-container .image7").hover(function(){
-    $(".galerie-container .image7 > div").css("display", "block");
-  }, function(){
-    $(".galerie-container .image7 > div").css("display", "none");
-  });
-
+    $(".galerie-container .image7").hover(function(){
+      $(".galerie-container .image7 > div").css("display", "block");
+    }, function(){
+      $(".galerie-container .image7 > div").css("display", "none");
+    });
+  }
 
 
 
